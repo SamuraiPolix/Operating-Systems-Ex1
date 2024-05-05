94ef564f96d9c0ed71b76a3e6bdf7633a2ee5aaf
SamuraiPolix <sam.lazareanu@gmail.com>
2024-05-05 14:20:06 +0300

    save work

---
+++ question-2/poisson.c
@@ -0,0 +1,13 @@
+#include <math.h>
+#include <stdio.h>
+
+long factorial(int num)
+
+int main(int argc, char* argv[]){
+    if (argc != 3){
+        printf(stderr, "Error\n");
+        exit(1);
+    }
+    
+    printf("%ld\n", pow((int)argv[1], (int)argv[2])/)
+}
